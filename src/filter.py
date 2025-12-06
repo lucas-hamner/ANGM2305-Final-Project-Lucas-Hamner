284a2e9e49c46de760d15b3d5d1490c53b560b33
ibriti10 <iiapextoastii@gmail.com>
2025-12-06 15:28:02 -0600

    Simple psuedocode added, beginning to write code.

---
+++ src/filter.py
@@ -0,0 +1,12 @@
+#Style FX
+
+#imports (pillow, sys, etc.)
+
+#image i/o
+
+#import filter/filters list for filter apply functions (sepia, grayscale, brighten, etc.)
+
+#gui formatting of window.
+
+#main()
+
