10e5736dd0b3eb1cf2230ec576f47fa100732f31
ibriti10 <iiapextoastii@gmail.com>
2025-12-08 14:35:04 -0600

    Added image loading/saving

--- src/filter.py
+++ src/filter.py
@@ -4,3 +4,9 @@
 from PIL import Image, ImageOps, ImageFilter, ImageEnhance, ImageTk
 import tkinter as tk
 from tkinter import filedialog, messagebox, ttk
+
+# IMAGE I/O
+def load_image(path):
+    return Image.open(path).convert("RGB")
+def save_image(img, path):
+    img.save(path)
